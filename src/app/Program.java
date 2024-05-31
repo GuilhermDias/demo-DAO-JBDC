43b454236c7ae034e474e34aa4d9c93c6f2e2157
Guilherme Dias <guilhermediasthomaz@hotmail.com>
2024-05-31 12:07:26 -0300

    SellerDaoJBDC and DaoFactory

--- src/app/Program.java
+++ src/app/Program.java
@@ -2,6 +2,8 @@ package app;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entites.Department;
 import model.entites.Seller;
 
@@ -13,6 +15,9 @@ public class Program {
 
 		Seller obj1 = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(obj1);
+		
+		SellerDao sellerdao = DaoFactory.createSellerDao();
+		
 	}
 
 }
